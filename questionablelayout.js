c0827a507453ea677c048603200c4048d9f1c91e
justinwerre <werrejustin@gmail.com>
2016-01-24 14:16:50 -0700

    Moved inline styles to their own style sheet.

--- questionablelayout.js
+++ questionablelayout.js
@@ -6,15 +6,7 @@ function fixLayout(){
 
 	// create a new div to hold the comic and news
 	var left_container = document.createElement("div");
-
-	// provide some spacing between comic and news and make sure it's width matches the comic's width
-	news.setAttribute("style", "margin-top:10px; width: 100%; padding: 0px");
-
-	// move the margin from the comic to the left container to insure the right side ads
-	// don't over lap the comics. set the display of the left-contaier to inline block so the
-	// comic can dictate the containers width.
-	comic.setAttribute("style", "margin-right: 0px");
-	left_container.setAttribute("style", "display: inline-block; margin-right: 217px");
+	left_container.id = "left_container";
 
 	// move the comic and news elements to the left container.
 	left_container.appendChild(comic);
